6d9ac76f60ae8cbf1aee73a062700ce2b40f0686
Diego Núñez Silva <diego.nunez.mx@gmail.com>
2023-11-05 22:02:47 +0100

    Initial commit

---
+++ src/main/java/dns/backup/BackupPcloudDataApplication.java
@@ -0,0 +1,13 @@
+package dns.backup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackupPcloudDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackupPcloudDataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dns/backup/BackupPcloudDataApplicationTests.java
@@ -0,0 +1,13 @@
+package dns.backup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackupPcloudDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
